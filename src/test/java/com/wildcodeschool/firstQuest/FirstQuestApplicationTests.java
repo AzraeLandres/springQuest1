26a7a09467a9f565b7ad42b8171a24053e1975c8
AzraeLandres <azraellandres@gmail.com>
2024-01-15 12:06:54 +0100

    first

---
+++ src/test/java/com/wildcodeschool/firstQuest/FirstQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.firstQuest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
